20866900d34a3349b1720020e279e00d5e49a5e8
LeenDeBaets <leendebaets@hotmail.com>
2013-07-12 17:09:10 +0200

    Edit page: the not required parameters must not be filled out

--- public/editResource.php
+++ public/editResource.php
@@ -44,20 +44,61 @@ $app->match('/ui/resource/edit{url}', function (Request $request) use ($app,$hos
 
 	$jsonobj = json_decode($obj);
 
+	// getting information about the required parameters a file
+	try {
+		if ($app['session']->get('userget') == null || $app['session']->get('pswdget') ==null) {
+			$request3 = $client->get('tdtinfo/admin.json');
+		} else {
+			$request3 = $client->get('tdtinfo/admin.json')->setAuth($app['session']->get('userget'),$app['session']->get('pswdget'));
+		}
+		$obj2 = $request3->send()->getBody();
+	 } catch (ClientErrorResponseException $e) {
+	 	if ($e->getResponse()->getStatusCode() == 401) {
+		 	$app['session']->set('method','get');
+		 	$app['session']->set('redirect',$hostname.'ui/package/add');
+		 	$app['session']->set('referer',$hostname.'ui/package/add');
+			return $app->redirect('../../ui/authentication');	
+	 	}
+	 }
+	$jsonobj2 = json_decode($obj2);
+
 	// iterating through all the parameters and the ones that are not edible will not be added
 	foreach ($jsonobj as $key => $value) {
 		foreach ($jsonobj->$key as $key2 => $value2) {
 			if(in_array($key2, $app['session']->get('notedible'))){
+				if ($key2 == 'generic_type') {
+					$type = $value2;
+				}
+				if ($key2 == 'resource_type') {
+					$generaltype = $value2;
+				}
 
 			}else{
 				$parameterstobechanged[$key2] = $value2;
 			}
 		}
 	}
+
+	// Getting the required parameters
+	if ( $generaltype == 'generic'){
+		$requiredcreatevariables = $jsonobj2->admin->create->generic->$type->requiredparameters;
+	}
+	else {
+		$requiredcreatevariables = $jsonobj2->admin->create->$generaltype->requiredparameters;
+	}
+
 	// Create a Silex form with all the fields to be changed and the fields already set on the value
 	$form = $app['form.factory']->createBuilder('form',$parameterstobechanged);
 	foreach ($parameterstobechanged as $key => $value) {
-		$form = $form->add($key,'text',array('constraints' => new Assert\NotBlank()));
+		// All the variables that are required
+		if (in_array($key, $requiredcreatevariables)) {
+			$form = $form->add($key,'text',array('constraints' => new Assert\NotBlank()));
+		}
+		// All the variables that are not required
+		else {
+			$form = $form->add($key,'text',array('required' => false));
+		}
+		
 	}
 
 	$form = $form->getForm();
