08bf0d4f23fc5c33ed02704d7c5210e6275fdbd6
Jeppe Knockaert <jeppe.knockaert@gmail.com>
2013-07-15 12:15:03 +0200

    Update for input

--- public/input.php
+++ public/input.php
@@ -13,15 +13,17 @@
 use Guzzle\Http\Client;
 // included for catching the 401 errors (authorization needed)
 use Guzzle\Http\Exception\ClientErrorResponseException;
+use Symfony\Component\HttpFoundation\Request;
 
-$app->get('/ui/input{url}', function () use ($app,$hostname,$data) {
+$app->match('/ui/input{url}', function (Request $request) use ($app,$hostname,$data) {
 	$fileMapping = @file_get_contents("/home/leen/Downloads/uitdb-events.xml.spec.ttl");
 
     $fileInput = @file_get_contents("/home/leen/Downloads/uitdb-events-test.xml");
     // echo "<pre>";
     // print_r($fileInput);
+    $defaultData = array('Input' => $fileInput,'Mapping' => $fileMapping);
 
-    $form = $app['form.factory']->createBuilder('form',array('Input' => $fileInput,'Mapping' => $fileMapping));
+    $form = $app['form.factory']->createBuilder('form',$defaultData);
     $form = $form->add('Input','textarea',array('attr' => array('cols' => "100", 'rows' => "200", 'style' => "width: 100%; height: 110px;")));
     $form = $form->add('Mapping','textarea',array('attr' => array('cols' => "100", 'rows' => "500")));
     $form = $form->getForm();
@@ -31,6 +33,30 @@ $app->get('/ui/input{url}', function () use ($app,$hostname,$data) {
 	$data['title']= "";
 	$data['header']= "";
 	$data['button']= "Change";
+
+    if ('POST' == $request->getMethod()){
+        $form->bind($request);
+        // Retrieve the form data
+        $formdata = $form->getData();
+
+        // Check the validity of the form
+        if ($form->isValid()){
+            // Button to save the file was clicked
+            if ($form->get('saveFile')->isClicked()){
+
+            }
+            // Button to save the mapping file was clicked
+            else if ($form->get('saveMappingFile')->isClicked()){
+
+            }
+            // Else, the usual submit button was used
+            else{
+                
+            }
+
+        }
+    }
+
 	return $app['twig']->render('form.twig', $data);
 	
 
