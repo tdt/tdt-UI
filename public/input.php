ea7522039e3e4b7534e1423d6dd85f374b949352
Jeppe Knockaert <jeppe.knockaert@gmail.com>
2013-07-15 15:21:26 +0200

    First two buttons on the input page are working

--- public/input.php
+++ public/input.php
@@ -14,27 +14,20 @@ use Guzzle\Http\Client;
 // included for catching the 401 errors (authorization needed)
 use Guzzle\Http\Exception\ClientErrorResponseException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormError;
 
-// Used to write json to file, formatted to be read by humans
-require_once __DIR__.'/../src/nicejson-php/nicejson.php';
 
 $app->match('/ui/input{url}', function (Request $request) use ($app,$hostname,$data) {
     // getting the input and mapping file (inserted by the user and saved in session-object)
 	$fileInput = @file_get_contents($app['session']->get('inputfile'));
     $fileMapping = @file_get_contents($app['session']->get('mappingfile'));
-    
-    $form = $app['form.factory']->createBuilder('form',array('Input' => $fileInput,'Mapping' => $fileMapping));
+
+    $form = $app['form.factory']->createBuilder('form',array('input' => $fileInput,'mapping' => $fileMapping));
     $form = $form->add('input','textarea',array('attr' => array('cols' => "100", 'rows' => "200", 'style' => "width: 100%; height: 110px;")));
     $form = $form->add('saveFile','submit');
     $form = $form->add('mapping','textarea',array('attr' => array('cols' => "100", 'rows' => "200", 'style' => "width: 100%; height: 110px;")));
     $form = $form->add('saveMappingFile','submit');
     $form = $form->getForm();
-
-    $data['form'] = $form->createView();
-	// adding the datafields title and function for the twig file
-	$data['title']= "";
-	$data['header']= "";
-	$data['button']= "Change";
 
     if ('POST' == $request->getMethod()){
         $form->bind($request);
@@ -43,27 +36,37 @@ $app->match('/ui/input{url}', function (Request $request) use ($app,$hostname,$d
 
         // Check the validity of the form
         if ($form->isValid()){
+            $write = false;
             // Button to save the file was clicked
             if ($form->get('saveFile')->isClicked()){
-                $filename = "/Volumes/Data/tmp/test.json";
-                $filtype = "JSON";
-                $error = writeToFile($filename,$formdata['input'],"json");
-                if ($error !== TRUE){
-                    return $error;
-                }
-                return "saveFile";
+                $fieldname = "input";
+                $write = true;
             }
             // Button to save the mapping file was clicked
             else if ($form->get('saveMappingFile')->isClicked()){
-                return "test2";
+                $fieldname = "mapping";
+                $write = true;
             }
             // Else, the usual submit button was used
             else{
                 return "test3";
             }
+            if ($write){
+                $filename = $app['session']->get($fieldname.'file');
+                $error = writeToFile($filename,$formdata[$fieldname]);
+                if ($error !== TRUE){
+                    $form->get($fieldname)->addError(new FormError($error));
+                }
+            }
 
         }
     }
+
+    $data['form'] = $form->createView();
+    // adding the datafields title and function for the twig file
+    $data['title']= "";
+    $data['header']= "";
+    $data['button']= "Change";
 
 	return $app['twig']->render('form.twig', $data);
 })->value('url', '');
@@ -72,22 +75,15 @@ $app->match('/ui/input{url}', function (Request $request) use ($app,$hostname,$d
  * Write data to file, taking into account the filetype for formatting
  * @param $file The filepath to write to
  * @param $data The textstring to write to the file
- * @param $type The type of formatting or null if no formatting is needed (json, xml or csv)
  * @return if success, TRUE is returned, else an errorstring is returned
  */
-function writeToFile($filepath,$data,$type = null){
+function writeToFile($filepath,$data){
     $filename = basename($filepath);
     $dirname = dirname($filepath);
     if (!is_writable($dirname)){
         return $dirname." is not writable";
     }
-    if (strcmp(trim(strtolower($type)),"json") === 0){
-        $returnval = file_put_contents($filename, json_format($data));
-    }
-    else{
-        $returnval = file_put_contents($filename, $data);
-    }
-    if ($returnval === FALSE){
+    if (file_put_contents($filepath, $data) === FALSE){
         return "Write to file ".$filename." failed";
     }
     return TRUE;
